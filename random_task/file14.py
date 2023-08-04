0260eb804b788d8b04a5af79babc2770f75a066c
AbdallahAg5 <agmarabdallah3@gmail.com>
2023-08-04 06:18:40 +0100

    How do you find the sum of n numbers in Python?

--- random_task/file14.py
+++ random_task/file14.py
@@ -1,8 +1,9 @@
 # Remove Characters at Even Indices
 def remove_even_indexes(string):
+    newString = str()
     for i in range(len(string)):
         if i % 2 == 0:
-            string.replace(string[i],'')
-    print(string)
+          newString = string.replace(string[i],'')
+    print(newString)
 
 remove_even_indexes('hello there')        
