c2ca505ae9570b18dbd80cd5c1e4fb568da1e0c6
AbdallahAg5 <agmarabdallah3@gmail.com>
2023-08-04 14:09:47 +0100

    # Check if a String Ends with a Suffix

--- random_task/file19.py
+++ random_task/file19.py
@@ -1,14 +1,9 @@
 # Check if a String Ends with a Suffix
 def check_prefix(string,prefix):
-    if len(prefix) == 1 and string[-1] == prefix:
-        print('the string you provided starts with :' + prefix)
-    elif len(prefix) == 1 and string[-1] != prefix: 
-        print('the string you provided does not  starts with : ' + prefix)
-    elif  len(prefix) != 1:
-            if string[-1:len(prefix)] == prefix:
-                  print('the string you provided starts with :' + prefix)
-            else:
-                  print('the string you provided does not  starts with : ' + prefix)
+    if string.endswith(prefix):
+     print('the string you provided end with :' + prefix)
+    else:
+     print('the string you provided does not  end with : ' + prefix)
                  
 
-check_prefix("hello there",'e')
+check_prefix("hello theree",'eeeee')
