869be366fb32bce09a61922da9c37e6992a2245f
AbdallahAg5 <agmarabdallah3@gmail.com>
2023-08-07 20:18:39 +0100

    sorting dasc

---
+++ random_task/file24.py
@@ -0,0 +1,12 @@
+movies = [
+    {"name": "Zack", "yearOfBirth": 2018},
+    {"name": "Zack", "yearOfBirth": 2017},
+    {"name": "Zack", "yearOfBirth": 2016},
+    {"name": "Zack", "yearOfBirth": 2015},
+    {"name": "Zack", "yearOfBirth": 2014},
+    {"name": "Zack", "yearOfBirth": 2013}
+]
+
+
+movies.sort(key=lambda movie: movie['yearOfBirth'],reverse=True)
+print(movies)
